ecfc2d3494e6feb87d989876246afde8a645c100
sakibZeem <anamulsakibzim22@gmail.com>
2024-02-10 16:29:10 +0600

    navbar and footer component added to the layout

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import NavBar from '../pages/shared/NavBar/NavBar';
+import Footer from '../pages/shared/Footer/Footer';
 
 const Main = () => {
     return (
         <>
-         <Outlet />  
+            <NavBar />
+            <Outlet />
+            <Footer />
         </>
     );
 };
